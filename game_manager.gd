b352c117560e35f48b39b9754bddbb9336f4477f
Zeph Requilme <zephrequilme40@gmail.com>
2025-10-15 19:37:43 +0800

    Improved the receiving by using actual physics to bring it accurately to the setter, added tilt to it based on the player positions, and made some small improvements

--- game_manager.gd
+++ game_manager.gd
@@ -4,80 +4,93 @@ extends Node2D
 
 var current_player_index := 0
 var current_player: CharacterBody2D
+var other_player: CharacterBody2D
 var players := {}
 var touches := 0
 
 func switch():
-    current_player.hide_arrow()
-    current_player.move(0)
-    current_player_index = ~current_player_index
-    current_player = players[players.keys()[current_player_index]]
-    current_player.show_arrow()
+	current_player.hide_arrow()
+	current_player.move(0)
+	other_player = players[players.keys()[current_player_index]]
+	current_player_index = ~current_player_index
+	current_player = players[players.keys()[current_player_index]]
+	current_player.show_arrow()
 
 func touch():
-    touches += 1
-    switch()
+	touches += 1
+	switch()
+
+func arc(direction, time):
+	var velocity_x = direction.x / time
+	var velocity_y = (direction.y + 0.5 * ball.get_gravity().y * time * time) / time
+	var impulse = (Vector2(velocity_x, -velocity_y) * ball.mass)
+	
+	return impulse
 
 func _ready() -> void:
-    players = {
-        "hitter": $Players/PlayerHitter,
-        "setter": $Players/PlayerSetter,
-    }
-    current_player = players["hitter"]
-    
+	players = {
+		"hitter": $Players/PlayerHitter,
+		"setter": $Players/PlayerSetter,
+	}
+	current_player = players["hitter"]
+	other_player = players["setter"]
+	
 func _physics_process(delta: float) -> void:
-    if Input.is_action_just_pressed("player_switch") and touches == 0:
-        switch()
-        
-    if Input.is_action_just_pressed("ui_accept") and current_player.is_on_floor():
-        current_player.jump()
-        
-    if Input.is_action_just_pressed("hit") and !current_player.is_on_floor() and current_player.collision == true:
-        var impulse: Vector2
-        var tilt = Input.get_axis("left", "right")
-        if tilt == 0:
-            impulse = Vector2(500, 200)
-        elif tilt == -1:
-            impulse = Vector2(500, 100)
-        elif tilt == 1:
-            impulse = Vector2(400, 300)
-        
-        ball.hit(impulse, current_player.get_node("Area2D").position - ball.position)
-        touch()
-    
-    if Input.is_action_just_pressed("receive") and current_player.is_on_floor() and current_player.collision == true:
-        var direction = (players["setter"].position - ball.position).normalized()
-        var forward_force = direction
-        var tilt := Input.get_axis("left", "right")
-        
-        if tilt == 0:
-            forward_force = direction * 0
-        elif tilt == -1:
-            forward_force = direction * -150
-        elif tilt == 1:
-            forward_force = direction * 150
-        
-        var upward_force = Vector2(0, -250)
-        var impulse = forward_force + upward_force
-        ball.receive(impulse)
-        touch()
-        
-    if Input.is_action_just_pressed("set") and current_player.collision == true:
-        var direction = (players["hitter"].position - ball.position).normalized()
-        var forward_force = direction
-        var tilt := Input.get_axis("left", "right")
-        
-        if tilt == 0:
-            forward_force = direction * 0
-        elif tilt == -1:
-            forward_force = direction * 150
-        elif tilt == 1:
-            forward_force = direction * -150
-        
-        var upward_force = Vector2(0, -250)
-        var impulse = forward_force + upward_force
-        ball.set_ball(impulse)
-        touch()
-        
-    var direction := Input.get_axis("left", "right")
-    current_player.move(direction)
+	if Input.is_action_just_pressed("player_switch") and touches == 0:
+		switch()
+		
+	if Input.is_action_just_pressed("ui_accept") and current_player.is_on_floor():
+		current_player.jump()
+		
+	if Input.is_action_just_pressed("hit") and !current_player.is_on_floor() and current_player.collisions["hit"] == true:
+		var impulse: Vector2
+		var tilt = Input.get_axis("left", "right")
+		if tilt == 0:
+			impulse = Vector2(500, 200)
+		elif tilt == -1:
+			impulse = Vector2(500, 100)
+		elif tilt == 1:
+			impulse = Vector2(400, 300)
+		
+		ball.hit(impulse, current_player.get_node("HitArea").position - ball.position)
+		touch()
+	
+	if Input.is_action_just_pressed("receive") and current_player.is_on_floor() and current_player.collisions["receive"] == true:
+		var target: CharacterBody2D
+		var tilt := Input.get_axis("left", "right")
+		
+		if tilt == -1:
+			if other_player.position.x < current_player.position.x:
+				target = other_player
+			else:
+				target = current_player
+		else:
+			if other_player.position.x > current_player.position.x:
+				target = other_player
+			else:
+				target = current_player # todo, this should receive towards the enemy setter
+		
+		var direction = (target.get_node("SetArea").global_position - ball.position)
+		var time = 2.5
+		ball.receive(arc(direction, time))
+		touch()
+		
+	if Input.is_action_just_pressed("set") and current_player.collisions["set"] == true:
+		var direction = (players["hitter"].position - ball.position).normalized()
+		var forward_force = direction
+		var tilt := Input.get_axis("left", "right")
+		
+		if tilt == 0:
+			forward_force = direction * 0
+		elif tilt == -1:
+			forward_force = direction * 150
+		elif tilt == 1:
+			forward_force = direction * -150
+		
+		var upward_force = Vector2(0, -250)
+		var impulse = forward_force + upward_force
+		ball.set_ball(impulse)
+		touch()
+		
+	var direction := Input.get_axis("left", "right")
+	current_player.move(direction)
--- player.gd
+++ player.gd
@@ -2,10 +2,14 @@ extends CharacterBody2D
 
 @onready var arrow: Sprite2D = $Arrow
 
-const JUMP_VELOCITY = -700.0
-const SPEED = 500.0
+const JUMP_VELOCITY = -800.0
+const SPEED = 600.0
 
-var collision := false
+var collisions := {
+	"hit": false,
+	"set": false,
+	"receive": false
+}
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -30,10 +34,20 @@ func show_arrow():
 func hide_arrow():
 	arrow.hide()
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "Ball":
-		collision = true
+func _on_hit_area_body_entered(body: Node2D) -> void:
+	collisions["hit"] = true
 
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body.name == "Ball":
-		collision = false
+func _on_hit_area_body_exited(body: Node2D) -> void:
+	collisions["hit"] = false
+
+func _on_set_area_body_entered(body: Node2D) -> void:
+	collisions["set"] = true
+
+func _on_set_area_body_exited(body: Node2D) -> void:
+	collisions["set"] = false
+
+func _on_receive_area_body_entered(body: Node2D) -> void:
+	collisions["receive"] =true
+
+func _on_receive_area_body_exited(body: Node2D) -> void:
+	collisions["receive"] = false
